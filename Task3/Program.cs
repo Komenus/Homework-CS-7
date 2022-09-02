cda793355e020b9c8cf7de171e1ee6cb9a647f24
dmitriyzheleznyak <komenus@gmail.com>
2022-09-02 14:06:28 +0300

    all task done

---
+++ Task3/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+double[,] array = GetArray(rows, columns, 10, 99);
+PrintArray(array);
+Console.WriteLine();
+FindAMean(array);
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+
+        }
+    }
+    return result;
+}
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        { Console.Write($"{inArray[i, j]} "); }
+        Console.WriteLine();
+    }
+}
+void FindAMean(double[,] array)
+{
+    // double sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+        sum = sum + array[i,j];
+        }
+        double result = sum / array.GetLength(1);
+        Console.Write($"{result:f2} "); 
+    }
+}    
